e2e38b0b527b5325600f6cab70729fd3d9f08385
pavani <pavaniaatla@gmail.com>
2025-10-20 20:57:17 +0530

    III-Sem-Cpp Lab

---
+++ Week 2/week 2.2.cpp
@@ -0,0 +1,15 @@
+#include<iostream> 
+#include <stdlib>  
+using namespace std; 
+inline int square(int x) 
+{      
+return x * x;    
+}  
+int main()     
+{      
+system(“color F0”);       
+cout << "Roll No: 25B21AI054" << endl;       
+int num = 3;      
+cout<< “”square of”” <<num <<”is” << square(num) << endl; 
+return 0;    
+}   
